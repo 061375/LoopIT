1e78f6fec07b4a9d90453f145a0d1c96763b8c72
Jeremy Heminger <webdev@Jeremys-Mac-mini.local>
2019-08-15 10:45:23 -0700

    first

--- LoopIT/Program.cs
+++ LoopIT/Program.cs
@@ -10,6 +10,32 @@ namespace LoopIT
     {
         static void Main(string[] args)
         {
+            string strArgTwo = "dir";
+            string strArgThree = "";
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Program requires at least one argument");
+                Console.WriteLine("[1] - path to folder to loop");
+                Console.WriteLine("[2] - target directory or file [dir,file] optional - default is dir");
+                Console.WriteLine("[3] - directories to skip - can be a single value or list delimitted by commas");
+            }
+            else
+            {
+                if (args.Length >= 2)
+                    strArgTwo = args[1];
+                if (args.Length == 3)
+                    strArgThree = args[2];
+
+                StackLoopDirs sld = new StackLoopDirs();
+
+                if (strArgTwo == "dir")
+                    sld.TraverseTreeDirs(args[0], strArgThree);
+                
+                if (strArgTwo == "file")
+                    sld.TraverseTreeFiles(args[0], strArgThree);
+                
+            }
         }
     }
 }
